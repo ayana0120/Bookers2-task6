5db2939a226d3b481784719b338ce8b0ebb26380
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-08 16:53:11 +0900

    [Add]非同期いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,14 @@
 class FavoritesController < ApplicationController
 
   def create
-  	@book = Book.find(params[:book_id])
-  	favorite = current_user.favorites.create(book_id: @book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.create(book_id: @book.id)
   end
 
   def destroy
-  	book = Book.find(params[:book_id])
-  	favorite = current_user.favorites.find_by(book_id: @book.id).destroy
-  end
-
-  private
-
-  def book_params
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
   end
 
 end
